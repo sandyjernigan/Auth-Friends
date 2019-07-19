7c5806354791228435beee9566c10f8e5a348079
Sandy <sandy.jernigan@gmail.com>
2019-07-18 23:35:22 -0400

    Login in works with logging in and logging out

--- friends/src/components/App.js
+++ friends/src/components/App.js
@@ -10,20 +10,8 @@ class App extends React.Component {
 
     return (
       <div className="App">
-        <header>
-          <nav>
-            <NavLink to="/">Home</NavLink>
-            <NavLink to="/friends">Friends</NavLink>
-          </nav>
-          {/* <p>{this.state.errMsg}</p> */}
-        </header>
-
         <PrivateRoute path="/" component={Home} />
         <Route exact path="/login" component={Login} />
-        <Route path="/friends" exact render={props => <Friends />} />
-        <Route path="/friends/:id" render={props => <Friend />} />
-        <Route path="/new" render={props => <CreateFriend />} />
-        <Route path="/update/:id" render={props => <UpdateFriend />} />
       </div>
     );
   }
--- friends/src/components/Home.js
+++ friends/src/components/Home.js
@@ -1,14 +1,42 @@
-import React from 'react';
-import { Link } from "react-router-dom"
+import React from 'react'
+import { Route, NavLink, withRouter } from "react-router-dom"
+import { Friend, Friends, CreateFriend, UpdateFriend } from "./friends/"
+import PrivateRoute from './PrivateRoute'
+import Login from './Login'
 
-export default function() {
-  return (
-    <div className="home">
-        <h2>Home</h2>
-        <Link to="/friends">
-            <button id="friendsbtn">Friends List</button></Link>
-        <Link to="/new">
-            <button id="friendsbtn">Add a New Friend</button></Link>
-    </div>
-  );
+class Home extends React.Component {
+  logout = (evt) => {
+    evt.preventDefault()
+
+    localStorage.removeItem('token')
+    this.props.history.push('/login')
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <nav>
+            <NavLink to="/">Home</NavLink>
+            <NavLink to="/friends">Friends</NavLink>
+            <NavLink to="/new">Add a New Friend</NavLink>
+            <button type="button" onClick={this.logout}>Logout</button>
+          </nav>
+          {/* <p>{this.state.errMsg}</p> */}
+        </header>
+        <div className="home">
+            <h2>Home</h2>
+        </div>
+
+        
+        <Route exact path="/login" component={Login} />
+        <Route path="/friends" exact render={props => <Friends />} />
+        <Route path="/friends/:id" render={props => <Friend />} />
+        <Route path="/new" render={props => <CreateFriend />} />
+        <Route path="/update/:id" render={props => <UpdateFriend />} />
+      </div>
+    );
+  }
 }
+
+export default withRouter(Home);
